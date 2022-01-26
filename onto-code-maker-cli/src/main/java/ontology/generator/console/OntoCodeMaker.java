d9cdf06d94934952b6d6d49a82963535ab6cb49d
Tomas <xsvetl05@stud.fit.vutbr.cz>
2022-01-26 23:14:59 +0100

    Initial commit

---
+++ onto-code-maker-cli/src/main/java/ontology/generator/console/OntoCodeMaker.java
@@ -0,0 +1,9 @@
+package ontology.generator.console;
+
+public class OntoCodeMaker {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to  OntoCodeMaker");
+    }
+}
